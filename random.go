ce77066cba6ac2ddc7ed73f14d3d37a7a0da5207
Mark Standen <mark@markstanden.dev>
2021-03-23 13:22:45 +0000

    bug fix, and added more verbose logging and comments to aid future self

--- random.go
+++ random.go
@@ -9,22 +9,25 @@ import (
 // modified version of example here:
 // https://golang.org/pkg/crypto/rand/#example_Read
 
+// getSecureByteSlice produces a slice of random bytes, of a provided length
+// used as the base for other outputs
 func getSecureByteSlice(length int) (bs []byte, err error) {
 	bs = make([]byte, length)
 	r, err := rand.Read(bs)
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("securerandom/getSecureByteSlice: failed to create secure slice of bytes \n%v", err)
 	}
 	if r != length {
-		return nil, fmt.Errorf("failed to create %v byte string", length)
+		return nil, fmt.Errorf("securerandom/getSecureByteSlice: failed to create %v byte string", length)
 	}
 	return bs, nil
 }
 
+// String returns a secure URL compliant string of a provided length
 func String(length int) (message string, err error) {
 	bs, err := getSecureByteSlice(length)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("securerandom/String: failed to create secure slice of bytes \n%v", err)
 	}
 	// encode to URL encoded base 64 string
 	// so it can be transmitted in a URL if required
