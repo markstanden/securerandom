7d6c3c56c5fae1c0c12e4a156af725ad56a2df91
Mark Standen <mark@markstanden.dev>
2021-03-23 19:15:00 +0000

    exported ByteSlice function to alow reuse in other projects

--- random.go
+++ random.go
@@ -9,23 +9,23 @@ import (
 // modified version of example here:
 // https://golang.org/pkg/crypto/rand/#example_Read
 
-// getSecureByteSlice produces a slice of random bytes, of a provided length
+// ByteSlice produces a slice of random bytes, of a provided length
 // used as the base for other outputs
-func getSecureByteSlice(length int) (bs []byte, err error) {
+func ByteSlice(length int) (bs []byte, err error) {
 	bs = make([]byte, length)
 	r, err := rand.Read(bs)
 	if err != nil {
-		return nil, fmt.Errorf("securerandom/getSecureByteSlice: failed to create secure slice of bytes \n%v", err)
+		return nil, fmt.Errorf("securerandom/ByteSlice: failed to create secure slice of bytes \n%v", err)
 	}
 	if r != length {
-		return nil, fmt.Errorf("securerandom/getSecureByteSlice: failed to create %v byte string", length)
+		return nil, fmt.Errorf("securerandom/ByteSlice: failed to create %v byte string", length)
 	}
 	return bs, nil
 }
 
 // String returns a secure URL compliant string of a provided length
 func String(length int) (message string, err error) {
-	bs, err := getSecureByteSlice(length)
+	bs, err := ByteSlice(length)
 	if err != nil {
 		return "", fmt.Errorf("securerandom/String: failed to create secure slice of bytes \n%v", err)
 	}
